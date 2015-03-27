6e66b7f77a9b36d74480da8d01e494a7106d1c5a
John Livingston <jckhnk@yahoo.com>
2015-03-26 20:14:49 -0700

    Checking in basic http auth

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,16 @@
 class Admin::AdminController < ApplicationController
+  
+  before_filter :authenticate
+
 
 	layout "admin"
+  
+  protected
+
+  def authenticate	
+  	authenticate_or_request_with_http_basic do |name, password|
+  		name == "admin" && password == "exoplanet"
+  	end
+  end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+
   protect_from_forgery
+
+  def authenticate
+  	authenticate_or_request_with_http_basic do |name, password|
+  		name == "spikes" && password == "exoplanet"
+  	end
+
+  end
+
 end
