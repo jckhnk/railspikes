8c91dcce5a9159dccfa7b1694f93bd79c2ce9551
Jacob Llamas <jllamas@ipac.caltech.edu>
2015-04-02 19:52:03 -0700

    moving url helper method into the view

--- app/controllers/kobs_controller.rb
+++ app/controllers/kobs_controller.rb
@@ -3,14 +3,14 @@ class KobsController < PlotsController
 	def gets
 		@title = "KOBs"
 		@model_name = Kob
-		@plot_route = Rails.application.routes.url_helpers.kobs_path
+		@plot_route = :kobs_path
 		super
 	end
 
 	def posts
 		@title = "KOBs"
 		@model_name = Kob
-		@plot_route = Rails.application.routes.url_helpers.kobs_path
+		@plot_route = :kobs_path
 		super
 	end
 
--- app/controllers/kois_controller.rb
+++ app/controllers/kois_controller.rb
@@ -3,14 +3,14 @@ class KoisController < PlotsController
 	def gets
 		@title = "KOIs"
 		@model_name = Koi
-		@plot_route = Rails.application.routes.url_helpers.kois_path
+		@plot_route = :kois_path
 		super
 	end
 
 	def posts
 		@title = "KOIs"
 		@model_name = Koi
-		@plot_route = Rails.application.routes.url_helpers.kois_path
+		@plot_route = :kois_path
 		super
 	end
 
