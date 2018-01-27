4f1deefa0d025edbde1c8b425bc0cde07ef33490
Jacob Llamas <jllamas@ipac.caltech.edu>
2018-01-26 23:39:01 -0800

    Removing code to upload older tables.
    Updated the forms to upload the new catalog csv, same as the rake task.

--- app/controllers/admin/uploads_controller.rb
+++ app/controllers/admin/uploads_controller.rb
@@ -1,67 +1,22 @@
-# def parse_csv(tmp)
-# 	f = []
-# 	File.open(tmp) do |f1|
-# 		while line = f1.gets
-# 			f << line.split(',')
-# 		end
-# 	end
-# 	f
-# end
-
 class Admin::UploadsController < Admin::AdminController
 
 	def get
 	end
 
-	def post
-  	## Upload method for KOI
-		require 'csv'
-    sources = []
-
-		csv_text = File.read(params['myfile'].tempfile.path)
-		csv = CSV.parse(csv_text, :headers => true)
-		csv.each do |row|
-		  sources << Koi.new(row.to_hash)
-		end
-
-  	Koi.delete_all
-  	sources.map{|s| s.save}
-
-		redirect_to admin_upload_path, :notice => "#{Koi.count} KOI Sources Ingested"
-	end
-
 	def posts
-  	## Upload method for KOB
 		require 'csv'
     sources = []
 
 		csv_text = File.read(params['myfile'].tempfile.path)
 		csv = CSV.parse(csv_text, :headers => true)
 		csv.each do |row|
-		  sources << Kob.new(row.to_hash)
+		  sources << KeplerStellarCatalog.new(row.to_hash)
 		end
 
-  	Kob.delete_all
+  	KeplerStellarCatalog.delete_all
   	sources.map{|s| s.save}
 
-		redirect_to admin_upload_path, :notice => "#{Kob.count} KOB Sources Ingested"
-	end
-
-	def puts
-  	## Upload method for KOB
-		require 'csv'
-    sources = []
-
-		csv_text = File.read(params['myfile'].tempfile.path)
-		csv = CSV.parse(csv_text, :headers => true)
-		csv.each do |row|
-		  sources << KeplerTarget.new(row.to_hash)
-		end
-
-  	KeplerTarget.delete_all
-  	sources.map{|s| s.save}
-
-		redirect_to admin_upload_path, :notice => "#{KeplerTarget.count} Kepler Targets Ingested"
+		redirect_to admin_upload_path, :notice => "#{KeplerStellarCatalog.count} Sources Ingested"
 	end
 
 end
