2ef993dc561119d3c949bb4cea7a265522228714
Jacob Llamas <jllamas@ipac.caltech.edu>
2018-02-12 16:39:24 -0800

    Updating download all data method to stream directly as a download rather than opening in the browser

--- app/controllers/download/kepler_targets_controller.rb
+++ app/controllers/download/kepler_targets_controller.rb
@@ -1,19 +1,9 @@
 class Download::KeplerTargetsController < ApplicationController
 
   def get
-
-		require 'csv'
-
-		csv = CSV.generate do |csv|
-			csv << (KeplerStellarCatalog.all_column_names)
-			KeplerStellarCatalog.all.each do |r|
-				csv << r.to_csv
-			end
-
-		end
-
-		send_data csv, :filename => "SpiKeS_Kepler_Stellar_Catalog_#{Time.now.strftime("%b_%d_%T")}.csv", :type=> 'text/csv' and return
-
+    filename = "SpiKeS-All-Sources-180211.csv"
+    data = File.read(Rails.root + "public/data" + filename)
+		send_data data, :filename => filename, :type => 'text/csv' and return
   end
 
 end
