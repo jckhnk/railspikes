d3eec8c60d4cfd4ab97a0a5139049a12be504ec8
Jacob Llamas <jllamas@ipac.caltech.edu>
2018-02-15 14:36:19 -0800

    Updating download all filename

--- app/controllers/download/kepler_targets_controller.rb
+++ app/controllers/download/kepler_targets_controller.rb
@@ -1,7 +1,7 @@
 class Download::KeplerTargetsController < ApplicationController
 
   def get
-    filename = "SpiKeS-All-Sources-180211.csv"
+    filename = "SpiKeS-All-Sources-180214.csv"
     data = File.read(Rails.root + "public/data" + filename)
 		send_data data, :filename => filename, :type => 'text/csv' and return
   end
